36730d0ad564140c7560f8f6cf51cb506653a97c
Theo van Oostrum <gt.oostrum@gmail.com>
2025-09-17 00:55:14 +0200

    title + deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ HOST=xwp71006-3@xwp30.xel.nl
 ssh "$HOST" rm -rf deploy && mkdir -p deploy
 scp -r ./* "$HOST":~/deploy
 ssh "$HOST" 'rm -rf clearblocks.nl/* && cp -r deploy/* clearblocks.nl'
+ssh "$HOST" 'rm -rf clearblocks.nl/deploy.sh clearblocks.nl/deploy'
 
 
 echo "\n" Done! Visit https://clearblocks.nl to view the changes "\n"
